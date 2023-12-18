f504b7ba558cbf955410cfd17207d4b2d87723b3
AndrewSkoba <a.skoba89@gmail.com>
2023-12-18 21:11:56 +0200

    For conflict

--- package/Main.java
+++ package/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("My first commit");
+        System.out.println("My first commi2t");
+        System.out.println("My first commit2");
+        System.out.println("2My first commit");
     }
 }
